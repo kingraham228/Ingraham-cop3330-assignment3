5ef22df1ad81df36f5d0bf6af36a4a4188bab46e
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-06-18 10:58:53 -0400

    Beginning pseudocode

---
+++ src/main/java/ex45/App.java
@@ -0,0 +1,15 @@
+package ex45;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Kate Ingraham
+ */
+public class App {
+    public static void main(String [] args){
+        //Ask user for the output file name
+        //Read text file
+        //search for "utilize"
+        //replace "utilize" with "use"
+        //write text to a new file with user-provided name
+
+    }
+}
